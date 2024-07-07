66fe2370ccbb2a44527780e6b93a97b507b8647b
lc-silverio <oppositedeep@gmail.com>
2024-07-07 23:53:48 +0100

    Added Jira service
    Added service to handle communications with Jira such as getting the users assigned tasks and update task time based on elapsed time

---
+++ TaskTickr/Services/Jira/JiraResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskTickr.Services.Jira
+{
+    /// <summary>
+    /// Defines the structure of a Jira response object
+    /// </summary>
+    public class JiraResponse
+    {
+        /// <summary>
+        /// Gets or sets the issues.
+        /// </summary>
+        /// <value>
+        /// The issues.
+        /// </value>
+        public List<JiraTask> Issues { get; set; }
+    }
+}
---
+++ TaskTickr/Services/Jira/JiraTask.cs
@@ -0,0 +1,32 @@
+﻿namespace TaskTickr.Services.Jira
+{
+    /// <summary>
+    /// Defines the structure of a Jira task object
+    /// </summary>
+    public class JiraTask
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key.
+        /// </summary>
+        /// <value>
+        /// The key.
+        /// </value>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Gets or sets the summary.
+        /// </summary>
+        /// <value>
+        /// The summary.
+        /// </value>
+        public string Summary { get; set; }
+    }
+}
